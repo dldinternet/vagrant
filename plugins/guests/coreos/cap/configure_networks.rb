ccd0ee0897845c88fc208aa93febfe1871eb43cf
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-08-03 22:53:31 +0200

    Make slaves connecting to first node

--- plugins/guests/coreos/cap/configure_networks.rb
+++ plugins/guests/coreos/cap/configure_networks.rb
@@ -10,39 +10,20 @@ module VagrantPlugins
 
         def self.configure_networks(machine, networks)
           machine.communicate.tap do |comm|
-<<<<<<< HEAD
             # Disable default etcd
             comm.sudo("systemctl stop etcd")
 
             # Read network interface names
-=======
-            # Remove any previous host only network additions to the interface file
-            comm.sudo("systemctl stop etcd")
-
-            primary_machine = machine.env.active_machines
-            #machine.env[:primary_machine].provider.driver.read_network_interfaces
-            #puts machine.env['admin1'].provider_config.network_adapters
-            #puts primary_machine
-
-            # Configure each network interface
->>>>>>> LVP for coreos guest support
             interfaces = []
             comm.sudo("ifconfig | grep enp0 | cut -f1 -d:") do |_, result|
               interfaces = result.split("\n")
             end
 
-<<<<<<< HEAD
             # Configure interfaces
             # FIXME: fix matching of interfaces with IP adresses
             networks.each do |network|
               comm.sudo("ifconfig #{interfaces[network[:interface].to_i]} #{network[:ip]} netmask #{network[:netmask]}")
             end
-=======
-            networks.each do |network|
-              comm.sudo("ifconfig #{interfaces[network[:interface].to_i]} #{network[:ip]} netmask #{network[:netmask]}")
-            end
-            puts 'TODO start etcd'
->>>>>>> LVP for coreos guest support
 
             primary_machine_config = machine.env.active_machines.first
             primary_machine = machine.env.machine(*primary_machine_config, true)
@@ -53,7 +34,6 @@ module VagrantPlugins
             end
 
             primary_machine_ip = get_ip.(primary_machine)
-<<<<<<< HEAD
             current_ip = get_ip.(machine)
             if current_ip == primary_machine_ip
               entry = TemplateRenderer.render("guests/coreos/etcd.service", :options => {
@@ -68,12 +48,12 @@ module VagrantPlugins
             end
 
             Tempfile.open("vagrant") do |temp|
-=======
-            if get_ip.(machine) == primary_machine_ip
-              entry = TemplateRenderer.render("guests/coreos/etcd.service")
             else
-              entry = TemplateRenderer.render("guests/coreos/etcd.service",
-                                              :options => {:connect_to => primary_machine_ip})
+              connection_string = "#{primary_machine_ip}:7001"
+              entry = TemplateRenderer.render("guests/coreos/etcd.service", :options => {
+                :connection_string => connection_string,
+                :my_ip => current_ip
+              })
             end
 
             puts entry
